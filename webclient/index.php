be1ed788cbdd5c01c89e295d89078f1710f9ea63
cyberjunky <support@domotiga.nl>
2009-03-23 11:40:51 +0000

    Better error checking to see if php xmlrpc module is installed.

--- webclient/index.php
+++ webclient/index.php
@@ -22,7 +22,12 @@ $configfile = 'config.php';
 if (file_exists($configfile)) {
    include "config.php";
 } else {
-   echo "<h2>Please configure me first by renaming <b>config.php.example</b> to <b>config.php</b> and check it's contents!</h2>";
+   echo "<h3>Check contents of config.php.example first, then rename it to config.php!</h3>";
+   exit;
+}
+if (!extension_loaded('xmlrpc')) {
+   echo "<h3>PHP xmlrpc module is not found, check your apache/php server setup!</h3>";
+   exit;
 }
 include "functions.php";
 import_request_variables("gp","r_");
