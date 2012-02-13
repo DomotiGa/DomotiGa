40e14f8bba0dfb8f184dd9300abc45b014b5458b
cyberjunky <support@domotiga.nl>
2012-02-13 12:01:37 +0000

    Added very early example of mobile webclient based on floorplan. Thanks to Gino.

---
+++ webclients/mobile/change_device.php
@@ -0,0 +1,40 @@
+<?php
+
+// DomotiGa - an open source home automation program
+// Copyright(C) 2008-2011 Ron Klinkien
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General PUBLIC License for more details.
+
+// You should have received a copy of the GNU General PUBLIC License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+$configfile = 'config.php';
+if (file_exists($configfile)) {
+   include "config.php";
+} else {
+   echo "<h3>Check contents of config.php.example first, then rename it to config.php!</h3>";
+   exit;
+}
+if (!extension_loaded('xmlrpc')) {
+   echo "<h3>PHP xmlrpc module is not found, check your apache/php server setup!</h3>";
+   exit;
+}
+include "functions.php";
+$device=$_GET["device"];
+$value=$_GET["value"];
+
+$request = xmlrpc_encode_request("device.setdevice",array ($device,$value));
+$response = do_xmlrpc($request);
+if (is_array($response) && xmlrpc_is_fault($response)) {
+   trigger_error("xmlrpc: $response[faultString] ($response[faultCode])");}
+
+header('Location: device.php'); 
+?>
