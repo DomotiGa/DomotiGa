244a4be930f90bd36763d7209231b963db9d75dc
cyberjunky <support@domotiga.nl>
2012-06-07 06:26:40 +0000

    Check for gambas install in compile.sh Group protocol buttons correctly in RFXComCmdr RX.

--- tools/compile.sh
+++ tools/compile.sh
@@ -1,38 +1,48 @@
 #!/bin/bash
 # Compile DomotiGa binaries
 
-if [ -d DomotiGaServer ]; then
-  cd DomotiGaServer
+hash gbc2 &> /dev/null
+if [ $? -eq 1 ]; then
+	echo "Gambas2 not found, skipping compile.."
 else
-  cd ../DomotiGaServer
-fi
-gbc2 -agm
-gba2 -o `pwd`/../DomotiGaServer.gambas
-rm -rf .gambas
-cd ..
-chmod +x DomotiGaServer.gambas
-ls -la DomotiGaServer.gambas
+	if [ -d DomotiGaServer ]; then
+	  cd DomotiGaServer
+	else
+	  cd ../DomotiGaServer
+	fi
+	gbc2 -agm
+	gba2 -o `pwd`/../DomotiGaServer.gambas
+	rm -rf .gambas
+	cd ..
+	chmod +x DomotiGaServer.gambas
+	ls -la DomotiGaServer.gambas
 
-if [ -d DomotiGa ]; then
-  cd DomotiGa
-else
-  cd ../DomotiGa
+	if [ -d DomotiGa ]; then
+	  cd DomotiGa
+	else
+	  cd ../DomotiGa
+	fi
+	gbc2 -agmp
+	gba2 -o `pwd`/../DomotiGa.gambas
+	rm -rf .gambas
+	cd ..
+	chmod +x DomotiGa.gambas
+	ls -la DomotiGa.gambas
 fi
-gbc2 -agmp
-gba2 -o `pwd`/../DomotiGa.gambas
-rm -rf .gambas
-cd ..
-chmod +x DomotiGa.gambas
-ls -la DomotiGa.gambas
 
-if [ -d DomotiGaServer3 ]; then
-  cd DomotiGaServer3
+hash gbc3 &> /dev/null
+if [ $? -eq 1 ]; then
+	echo "Gambas3 not found, skipping compile.."
 else
-  cd ../DomotiGaServer3
+	if [ -d DomotiGaServer3 ]; then
+	  cd DomotiGaServer3
+	else
+	  cd ../DomotiGaServer3
+	fi
+	gbc3 -agm
+	gba3 -o `pwd`/../DomotiGaServer3.gambas
+	rm -rf .gambas
+	cd ..
+	chmod +x DomotiGaServer3.gambas
+	ls -la DomotiGaServer3.gambas
 fi
-gbc3 -agm
-gba3 -o `pwd`/../DomotiGaServer3.gambas
-rm -rf .gambas
-cd ..
-chmod +x DomotiGaServer3.gambas
-ls -la DomotiGaServer3.gambas
