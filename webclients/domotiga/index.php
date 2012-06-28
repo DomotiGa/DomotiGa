6adf7590ac0d2042ee03c3b4cb72f68a24b6e7a2
cyberjunky <support@domotiga.nl>
2012-06-28 17:51:28 +0000

    Made tabs work again, thanks Giuseppe!

--- webclients/domotiga/index.php
+++ webclients/domotiga/index.php
@@ -66,7 +66,6 @@ if ( isset($r_action))
    header("Location: index.php?setview=control");
 }
 
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
