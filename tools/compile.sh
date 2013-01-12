02a0b2cb3d12c36322b4ecd43a00ae4157d4c60f
cyberjunky <support@domotiga.nl>
2013-01-12 14:14:58 +0000

    Added DomotiGa3 project to compile script.

--- tools/compile.sh
+++ tools/compile.sh
@@ -46,3 +46,20 @@ else
 	chmod +x DomotiGaServer3.gambas
 	ls -la DomotiGaServer3.gambas
 fi
+
+hash gbc3 &> /dev/null
+if [ $? -eq 1 ]; then
+	echo "Gambas3 not found, skipping compile.."
+else
+	if [ -d DomotiGa3 ]; then
+	  cd DomotiGa3
+	else
+	  cd ../DomotiGa3
+	fi
+	gbc3 -agm
+	gba3 -o `pwd`/../DomotiGa3.gambas
+	rm -rf .gambas
+	cd ..
+	chmod +x DomotiGa3.gambas
+	ls -la DomotiGa3.gambas
+fi
