812ff845d8a28452124ccb573b38eb00758dbc78
ualex73 <ualex73@gmail.com>
2013-11-23 18:26:26 +0000

    Fixed: Issue 373 - Serial Port usage form fails to open Fixed: Default settings_main table parameters were NULL Added: settings_main Plugin and GlobalVar debug options Changed: Removed not used GlobalX10 from RFXComTRX settings Changed: Aligned RFXComTRX, TX and RX settings

--- upgrade/10012to10013.sql
+++ upgrade/10012to10013.sql
@@ -4,6 +4,12 @@
 --
 UPDATE `triggers` SET `param2` = null, `param3` = null, `param4` = null WHERE `type` = 8;
 
+--
+-- Update settings_main table
+--
+UPDATE `settings_main` SET `debugplugin`=0 WHERE `debugplugin`=NULL;
+UPDATE `settings_main` SET `debugglobalvar`=0 WHERE `debugglobalvar`=NULL;
+UPDATE `settings_main` SET `autodevicecreate`=0 WHERE `autodevicecreate`=NULL;
 
 --
 -- Finally update to 1.0.013
