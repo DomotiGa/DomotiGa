478451998d32d7299236057f071ca9c226070e57
Ron Klinkien <ron@cyberjunky.nl>
2014-06-24 10:37:04 +0200

    Development branch for upnp devicediscovery.

--- upgrade/10015to10016.sql
+++ upgrade/10015to10016.sql
@@ -1,3 +1,30 @@
+--
+-- Table structure for table `settings_devicediscover`
+--
+DROP TABLE IF EXISTS `settings_devicediscover`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings_devicediscover` (
+  `id` int(11) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `multicasthost` varchar(32) DEFAULT NULL,
+  `multicastport` int(11) NOT NULL DEFAULT '0',
+  `localip` varchar(32) DEFAULT NULL,
+  `listenonly` tinyint(1) NOT NULL DEFAULT '0',
+  `broadcastinterval` int(11) NOT NULL DEFAULT '0',
+  `debug` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings_viera`
+--
+/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `settings_devicediscover` WRITE;
+/*!40000 ALTER TABLE `settings_devicediscover` DISABLE KEYS */;
+INSERT INTO `settings_devicediscover` VALUES (0,0,'239.255.255.250',1900,'192.168.178.80',0,60,0),(1,1,'239.255.255.250',1900,'192.168.178.80',0,60,0);
+/*!40000 ALTER TABLE `settings_devicediscover` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Add new protocols to RFXtrx interface
