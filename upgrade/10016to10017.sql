be51258735fb48b3562eefd33fbf5b77032a1bc8
jessekerkhoven <jesse.kerkhoven@gmail.com>
2014-08-08 13:02:30 +0200

    Fix discovery for sonos and, save data to collection

--- upgrade/10016to10017.sql
+++ upgrade/10016to10017.sql
@@ -1,4 +1,31 @@
 --
+-- Table structure for table `settings_devicediscover`
+--
+DROP TABLE IF EXISTS `settings_devicediscover`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `settings_devicediscover` (
+  `id` int(11) NOT NULL,
+  `enabled` tinyint(1) NOT NULL DEFAULT '0',
+  `multicasthost` varchar(32) DEFAULT NULL,
+  `multicastport` int(11) NOT NULL DEFAULT '0',
+  `listenonly` tinyint(1) NOT NULL DEFAULT '0',
+  `broadcastinterval` int(11) NOT NULL DEFAULT '0',
+  `debug` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings_viera`
+--
+/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES `settings_devicediscover` WRITE;
+/*!40000 ALTER TABLE `settings_devicediscover` DISABLE KEYS */;
+INSERT INTO `settings_devicediscover` VALUES (0,0,'239.255.255.250',1900,0,60,0),(1,1,'239.255.255.250',1900,0,60,0);
+/*!40000 ALTER TABLE `settings_devicediscover` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Add new protocols to RFXtrx interface
 --
 UPDATE interfaces SET type = 'X10 X10Security Oregon KAKU RFXCom AC HEUK ATI Digimax Mertik Ninja Flamingo Waveman HEEU ARC HE105 Koppla RTS10 Harrison Anslut Impuls AB400 EMW200 LightwaveRF TFA LaCrosse UPM Cresta Viking Rubicson RisingSun PhilipsSBC EMW100 BBSB Blyss RollerTrol HastaNew HastaOld A-OKRF01 A-OKAC114 Meiantech ByronSX SA30 X10SecDW X10SecMotion X10SecRemote PowerCodeDW PowerCodeMotion PowerCodeAux CodeSecure Energenie Livolo RSL TRC02 MDRemote SF01 RFY RFYEXT' WHERE id=35;
