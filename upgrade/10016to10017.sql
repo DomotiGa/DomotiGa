20f7820c66281b47bb00dafe61500b2c5e4187f5
jessekerkhoven <jesse.kerkhoven@gmail.com>
2014-08-08 20:58:15 +0200

    Fixed, remove old updgrade data

--- upgrade/10016to10017.sql
+++ upgrade/10016to10017.sql
@@ -16,7 +16,7 @@ CREATE TABLE `settings_devicediscover` (
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `settings_viera`
+-- Dumping data for table `settings_devicediscover`
 --
 /*!40101 SET character_set_client = @saved_cs_client */;
 LOCK TABLES `settings_devicediscover` WRITE;
@@ -26,61 +26,9 @@ INSERT INTO `settings_devicediscover` VALUES (0,0,'239.255.255.250',1900,0,60,0)
 UNLOCK TABLES;
 
 --
--- Add new protocols to RFXtrx interface
---
-UPDATE interfaces SET type = 'X10 X10Security Oregon KAKU RFXCom AC HEUK ATI Digimax Mertik Ninja Flamingo Waveman HEEU ARC HE105 Koppla RTS10 Harrison Anslut Impuls AB400 EMW200 LightwaveRF TFA LaCrosse UPM Cresta Viking Rubicson RisingSun PhilipsSBC EMW100 BBSB Blyss RollerTrol HastaNew HastaOld A-OKRF01 A-OKAC114 Meiantech ByronSX SA30 X10SecDW X10SecMotion X10SecRemote PowerCodeDW PowerCodeMotion PowerCodeAux CodeSecure Energenie Livolo RSL TRC02 MDRemote SF01 RFY RFYEXT' WHERE id=35;
-
---
--- Dumping data for table interfaces
-
-LOCK TABLES interfaces WRITE;
-/*!40000 ALTER TABLE interfaces DISABLE KEYS */;
-INSERT INTO interfaces VALUES  (57,'Wirelesstags Interface','Wirelesstags','Read Write');
-/*!40000 ALTER TABLE interfaces ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-INSERT INTO `devicetypes` VALUES (366,'Somfy Blind','Blind Remote','RFY','0xf12ab 1','','',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `devicetypes` VALUES (367,'Somfy Blind Ext','Blind Remote Ext','RFYEXT','0xf12ab 1','','',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
-INSERT INTO `devicetypes` VALUES (368,'Motion sensor','Motion Sensor','Wirelesstags','xxxxxxxxxx','','','',0,0,0,0,0,0,0);
-INSERT INTO `devicetypes` VALUES (369,'Water Soil Moisture sensor','Water Sensor','Wirelesstags','xxxxxxxxxx','','','',0,0,0,0,0,0,0);
-INSERT INTO `devicetypes` VALUES (370,'Reed sensor','Reed Sensor','Wirelesstags','xxxxxxxxxx','','','',0,0,0,0,0,0,0);
-INSERT INTO `devicetypes` VALUES (371,'PIR Sensor Tag','PIR Sensor','Wirelesstags','xxxxxxxxxx','','','',0,0,0,0,0,0,0);
-INSERT INTO `devicetypes` VALUES (372,'KumoStat Sensor','Thermostat','Wirelesstags','xxxxxxxxxx','','','',0,0,0,0,0,0,0);
-
---
--- Table structure for table settings_wirelesstags--
-DROP TABLE IF EXISTS settings_wirelesstags;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE settings_wirelesstags (
-  id int(11) NOT NULL,
-  enabled tinyint(1) NOT NULL DEFAULT '0',
-  polltime int(11) NOT NULL DEFAULT '0',
-  user varchar(64) DEFAULT NULL,
-  password varchar(64) DEFAULT NULL,
-  debug tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table settings_wirelesstags --
-
-LOCK TABLES settings_wirelesstags WRITE;
-/*!40000 ALTER TABLE settings_wirelesstags DISABLE KEYS */;
-INSERT INTO settings_wirelesstags VALUES (0,0,300,'username@domain.com','password',0),(1,0,300,'username@domain.com','password',0);
-/*!40000 ALTER TABLE settings_wirelesstags ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Update Boolean '1' to '-1'
---
-UPDATE devices SET enabled = '-1' WHERE enabled=1;
-
---
--- Finally update to 1.0.016
+-- Finally update to 1.0.017
 --
 LOCK TABLES version WRITE;
-INSERT INTO version VALUES (61,'1.0.016');
+INSERT INTO version VALUES (62,'1.0.017');
 UNLOCK TABLES;
 
