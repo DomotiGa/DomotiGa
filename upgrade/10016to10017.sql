5685650c945807569652a0eacf6c6b5ce5b0a292
jessekerkhoven <jesse.kerkhoven@gmail.com>
2014-08-31 11:37:41 +0200

    Remove settings table for weatherbug

---
+++ upgrade/10016to10017.sql
@@ -0,0 +1,12 @@
+--
+-- Table structure for table `settings_weatherbug`
+--
+DROP TABLE IF EXISTS `settings_weatherbug`;
+
+--
+-- Finally update to 1.0.017
+--
+LOCK TABLES version WRITE;
+INSERT INTO version VALUES (62,'1.0.017');
+UNLOCK TABLES;
+
