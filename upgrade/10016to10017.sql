d7115379c882f427b63c69b722a97fec5ac0ff85
Ron Klinkien <ron@cyberjunky.nl>
2014-09-02 13:36:48 +0200

    Fixed: Removed WeatherBug code. Added: WiringPi support, thanks Evgeny! (I only enhanced it a little)

--- upgrade/10016to10017.sql
+++ upgrade/10016to10017.sql
@@ -1,4 +1,29 @@
 --
+-- Table structure for settings_wiringpi
+--
+DROP TABLE IF EXISTS settings_wiringpi;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE settings_wiringpi (
+  id int(11) NOT NULL,
+  enabled tinyint(1) NOT NULL DEFAULT '0',
+  polltime int(11) NOT NULL DEFAULT '10',
+  debug tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `settings_wiringpi`
+--
+/*!40101 SET character_set_client = @saved_cs_client */;
+LOCK TABLES settings_wiringpi WRITE;
+/*!40000 ALTER TABLE `settings_wiringpi` DISABLE KEYS */;
+INSERT INTO settings_wiringpi VALUES
+(0,0,10,0),(1,0,10,0);
+/*!40000 ALTER TABLE `settings_wiringpi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for settings_pushbullet
 --
 DROP TABLE IF EXISTS settings_pushbullet;
@@ -97,12 +122,16 @@ UNLOCK TABLES;
 LOCK TABLES interfaces WRITE;
 /*!40000 ALTER TABLE interfaces DISABLE KEYS */;
 INSERT INTO interfaces VALUES  (58,'Philips Hue Interface','PhilipsHue','Read Write');
+INSERT INTO interfaces VALUES  (59,'WiringPi Interface','WiringPi','Read Write');
 /*!40000 ALTER TABLE interfaces ENABLE KEYS */;
 UNLOCK TABLES;
 
-
 INSERT INTO `devicetypes` VALUES (373,'Philips Hue Light Bulb','Light','PhilipsHue','1','','','',-1,-1,0,0,0,0,0);
 
+INSERT INTO `devicetypes` VALUES (374,'RaspberryPi I/O','RaspberryPi I/O','WiringPi','<pin>','','','',-1,-1,0,0,0,0,0);
+INSERT INTO `devicetypes` VALUES (375,'WiringPi I/O','WiringPi I/O','WiringPi','<wiringpi>:<baseaddr>:<i2caddr>|<pin>','','','',-1,-1,0,0,0,0,0);
+INSERT INTO `devicetypes` VALUES (376,'WiringPi I/O Inverted','WiringPi I/O Inverted','WiringPi','<wiringpi>:<baseaddr>:<i2caddr>|<pin>','','','',-1,-1,0,0,0,0,0);
+INSERT INTO `devicetypes` VALUES (377,'RaspberryPi I/O Inverted','RaspberryPi I/O Inverted','WiringPi','<pin>','','','',-1,-1,0,0,0,0,0);
 --
 -- Finally update to 1.0.017
 --
