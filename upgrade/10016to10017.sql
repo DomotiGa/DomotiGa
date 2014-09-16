61b2a5fcb9dbfed5562bbdf16ee2ae99a44ad4ea
Ron Klinkien <ron@cyberjunky.nl>
2014-09-16 10:55:21 +0200

    Added: Devicetypes for WiringPi ADC and DAC inputs/outputs.

--- upgrade/10016to10017.sql
+++ upgrade/10016to10017.sql
@@ -259,6 +259,9 @@ INSERT INTO `devicetypes` VALUES (375,'WiringPi I/O','WiringPi I/O','WiringPi','
 INSERT INTO `devicetypes` VALUES (376,'WiringPi I/O Inverted','WiringPi I/O Inverted','WiringPi','<wiringpi>:<baseaddr>:<i2caddr>|<pin>','','','',-1,-1,0,0,0,0,0);
 INSERT INTO `devicetypes` VALUES (377,'RaspberryPi I/O Inverted','RaspberryPi I/O Inverted','WiringPi','<pin>','','','',-1,-1,0,0,0,0,0);
 
+INSERT INTO `devicetypes` VALUES (382,'WiringPi ADC','WiringPi ADC','WiringPi','<wiringpi>:<baseaddr>:<i2caddr>|<pin>','','','',-1,-1,0,0,0,0,0);
+INSERT INTO `devicetypes` VALUES (383,'WiringPi DAC','WiringPi DAC','WiringPi','<wiringpi>:<baseaddr>:<i2caddr>|<pin>','','','',-1,-1,0,0,0,0,0);
+
 --
 -- Cleanup the OpenZWave/RaZberry devicetypes
 --
