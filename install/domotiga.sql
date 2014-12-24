503dc04738c1f0f081ddbc47d06d35c46970ad51
Alexie <ualex73@gmail.com>
2014-12-24 14:05:36 +0100

    Fixed: Table "thermostat_scenarii" column id wasn't renamed in install/upgrade files

--- install/domotiga.sql
+++ install/domotiga.sql
@@ -4523,10 +4523,10 @@ DROP TABLE IF EXISTS `thermostat_scenarii`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `thermostat_scenarii` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `scenario_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
   `description` text,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`scenario_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
