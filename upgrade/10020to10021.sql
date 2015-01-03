f268eef347bcb13797ea128d21fd395ff5c5e1ff
Ron Klinkien <ron@cyberjunky.nl>
2015-01-03 14:40:29 +0100

    Added: Support for SelectPlus doorbell fo RFXTrxE with firmware 234. Added: Few devicetype icons Changed: Enlarged protocols field inside plugins table.

--- upgrade/10020to10021.sql
+++ upgrade/10020to10021.sql
@@ -1,5 +1,19 @@
 
 --
+-- Make plugins protocols field bigger
+--
+
+ALTER TABLE plugins CHANGE COLUMN `protocols` `protocols` VARCHAR(1024) CHARACTER SET 'utf8' NOT NULL;
+
+--
+-- Add SelectPlus Chime for RFXTrx
+--
+
+UPDATE plugins SET protocols = 'X10 X10Security Oregon KAKU RFXCom AC HEUK ATI Digimax Mertik Ninja Flamingo Waveman HEEU ARC HE105 Koppla RTS10 Harrison Anslut Impuls AB400 EMW200 LightwaveRF TFA LaCrosse UPM Cresta Viking Rubicson RisingSun PhilipsSBC EMW100 BBSB Blyss RollerTrol HastaNew HastaOld A-OKRF01 A-OKAC114 Meiantech ByronSX ByronMP SA30 X10SecDW X10SecMotion X10SecRemote PowerCodeDW PowerCodeMotion PowerCodeAux CodeSecure Energenie Livolo RSL TRC02 MDRemote SF01 RFY RFYEXT Imagintrx WT TRC022 AOKE EuroDomest Smartwares SelectPlus' WHERE id = 35;
+
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (307,'SelectPlus Doorbell','Doorbell','SelectPlus','00F4');
+
+--
 -- Finally update to 1.0.021
 --
 
