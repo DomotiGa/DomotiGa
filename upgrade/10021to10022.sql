a87bfb91d2c39f0991bae143f8c0649910f0e62c
BreFra <frank_van_breugel@hotmail.com>
2015-06-26 15:41:32 +0200

    Added: Support for Velbus module VMB4RYLD

--- upgrade/10021to10022.sql
+++ upgrade/10021to10022.sql
@@ -6,3 +6,8 @@
 LOCK TABLES version WRITE;
 INSERT INTO version VALUES (67,'1.0.022');
 UNLOCK TABLES;
+
+--
+-- Add VMB4RYLD for Velbus
+--
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (671,'VMB4RYLD','Velbus - 4 Channel relay module','Velbus','13|2');
