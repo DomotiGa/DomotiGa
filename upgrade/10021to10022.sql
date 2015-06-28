9726e84a9553ff6a4f6bd7bd1576882bc122659c
Ron Klinkien <ron@cyberjunky.nl>
2015-06-28 15:09:56 +0200

    Small sql update and install corrections for Velbus.

--- upgrade/10021to10022.sql
+++ upgrade/10021to10022.sql
@@ -1,3 +1,7 @@
+--
+-- Add VMB4RYLD for Velbus
+--
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (671,'VMB4RYLD','Velbus - 4 Channel relay module','Velbus','13|2');
 
 --
 -- Finally update to 1.0.022
@@ -6,8 +10,3 @@
 LOCK TABLES version WRITE;
 INSERT INTO version VALUES (67,'1.0.022');
 UNLOCK TABLES;
-
---
--- Add VMB4RYLD for Velbus
---
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (671,'VMB4RYLD','Velbus - 4 Channel relay module','Velbus','13|2');
