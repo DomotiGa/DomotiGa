3b1e4fce102b13f6f4daafd0cbfd13241890a5a1
Alexie <ualex73@gmail.com>
2016-01-25 17:05:36 +0100

    Changed: Put upgrade fix for issue 589 back

--- upgrade/10022to10023.sql
+++ upgrade/10022to10023.sql
@@ -1,7 +1,18 @@
 --
 -- Delete duplicate Wirelesstags Plugin
 --
+
 DELETE FROM plugins WHERE id=93;
+
+--
+-- Fix 10020 to 10021 upgrade problem
+--
+
+DELETE FROM plugins WHERE id=94;
+DELETE FROM plugins WHERE id=95;
+
+INSERT INTO plugins (`id`, `interface`, `name`, `type`) VALUES (94, 'GPS', 'GPS', 'class');
+INSERT INTO plugins (`id`, `interface`, `protocols`, `name`, `type`) values (95,'Buienradar','','Buienradar','class');
 
 --
 -- extend serialport field length
