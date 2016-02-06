fca2ffe5455e46bc80d02658b724a1c2d50acead
Ron Klinkien <ron@cyberjunky.nl>
2016-02-06 18:30:13 +0100

    Added: Blinds1 support for RFXComTRX plugin. (needs testing) Added: Updated RFXCommander for Blinds1 control.

--- upgrade/10022to10023.sql
+++ upgrade/10022to10023.sql
@@ -52,7 +52,31 @@ ALTER TABLE plugins CHANGE COLUMN `protocols` `protocols` VARCHAR(1024) CHARACTE
 --
 -- Add Blinds1 support to rfxcom tranceiver plugin
 --
-UPDATE plugins SET protocols = 'X10 X10Security Oregon KAKU RFXCom AC HEUK ATI Digimax Mertik Ninja Flamingo Waveman HEEU ARC HE105 Koppla RTS10 Harrison Anslut Impuls AB400 EMW200 LightwaveRF TFA LaCrosse UPM Cresta Viking Rubicson RisingSun PhilipsSBC EMW100 BBSB Blyss BlindsT0 BlindsT1 BlindsT2 BlindsT3 BlindsT4 BlindsT5 BlindsT6 BlindsT7 BlindsT8 BlindsT9 BlindsT10 Meiantech ByronSX ByronMP SA30 X10SecDW X10SecMotion X10SecRemote PowerCodeDW PowerCodeMotion PowerCodeAux CodeSecure Energenie Livolo RSL TRC02 MDRemote SF01 RFY RFYEXT Imagintrx WT TRC022 AOKE EuroDomest Smartwares SelectPlus' WHERE id = 35;
+UPDATE plugins SET protocols = 'X10 X10Security Oregon KAKU RFXCom AC HEUK ATI Digimax Mertik Ninja Flamingo Waveman HEEU ARC HE105 Koppla RTS10 Harrison Anslut Impuls AB400 EMW200 LightwaveRF TFA LaCrosse UPM Cresta Viking Rubicson RisingSun PhilipsSBC EMW100 BBSB Blyss BlindsT0 BlindsT1 BlindsT2 BlindsT3 BlindsT4 BlindsT5 BlindsT6 BlindsT7 BlindsT8 BlindsT9 BlindsT10 BlindsT11 Meiantech ByronSX ByronMP SA30 X10SecDW X10SecMotion X10SecRemote PowerCodeDW PowerCodeMotion PowerCodeAux CodeSecure Energenie Livolo RSL TRC02 MDRemote SF01 RFY RFYEXT Imagintrx WT TRC022 AOKE EuroDomest Smartwares SelectPlus' WHERE id = 35;
+
+--
+-- Add Blinds1 devicetypes
+--
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (680,'Bofu Blinds','Blinds Motor','BlindsT0','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (681,'RollerTrol Blinds','Blinds Motor','BlindsT0','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (682,'Hasta New Blinds','Blinds Motor','BlindsT0','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (683,'Hasta Old Blinds','Blinds Motor','BlindsT1','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (684,'A-OK RF01 Blinds','Blinds Motor','BlindsT2','0x00a1b2');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (685,'A-OK AC114/123 Blinds','Blinds Motor','BlindsT3','0x00a1b2');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (686,'Raex YR1326 Blinds','Blinds Motor','BlindsT4','0x00a1b2');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (687,'Media Mount','Projection Screen','BlindsT5','0x00a1b2');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (688,'DC106 Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (689,'Rohrmotor24-RMF','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (690,'Yooda Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (691,'Dooya Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (692,'ESMO Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (693,'Brel Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (694,'Quitidom Motor','Blinds Motor','BlindsT6','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (695,'Forest Curtain','Blinds Motor','BlindsT7','0x00a1b2c 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (696,'Chamberlain CS4330CN','Blinds Motor','BlindsT8','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (697,'Sunpery/BTX Motor','Blinds Motor','BlindsT9','0x00a1b2 1');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (698,'Dolat DLM-1 Motor','Blinds Motor','BlindsT10','0x00a1b2');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) values (699,'Topstar Motor','Blinds Motor','BlindsT10','0x00a1b2');
 
 --
 -- Finally update to 1.0.023
