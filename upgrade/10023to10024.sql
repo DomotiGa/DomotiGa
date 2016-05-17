7d5d394f600aa1f9afc662fc52045f324b681da4
Alexie <ualex73@gmail.com>
2016-05-17 22:37:02 +0200

    Added: More velbus modules

--- upgrade/10023to10024.sql
+++ upgrade/10023to10024.sql
@@ -5,9 +5,21 @@
 
 DELETE FROM devicetypes WHERE id=711;
 DELETE FROM devicetypes WHERE id=712;
+DELETE FROM devicetypes WHERE id=713;
+DELETE FROM devicetypes WHERE id=714;
+DELETE FROM devicetypes WHERE id=715;
+DELETE FROM devicetypes WHERE id=716;
+DELETE FROM devicetypes WHERE id=717;
+DELETE FROM devicetypes WHERE id=718;
 
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (711, 'VMBDMI', 'Velbus - dimmer for resistive or inductive load', 'Velbus', '\'02\', \'05\' or \'12\'');
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (712, 'VMBPIRC', 'Velbus - Ceiling PIR detector module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (713, 'VMBPIRO', 'Velbus - Outdoor motion, twilight and temperature sensor', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (714, 'VMB8PBU', 'Velbus - 8-channel push-button module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (715, 'VMB6PBN', 'Velbus - Push-button interface for 6-/4-fold niko push-buttons', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (716, 'VMBGP2', 'Velbus - two touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (717, 'VMBGP4', 'Velbus - four touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (718, 'VMBGPOD', 'Velbus - Touch panel with Oled display', 'Velbus', '\'02\', \'05\' or \'12\'');
 
 --
 -- Finally update to 1.0.024
