613d2ed467ca0705f0c08ce9985db391eaccfeb4
Alexie <ualex73@gmail.com>
2016-05-19 22:50:53 +0200

    Changed: Updated Velbus devices

--- upgrade/10023to10024.sql
+++ upgrade/10023to10024.sql
@@ -11,6 +11,7 @@ DELETE FROM devicetypes WHERE id=715;
 DELETE FROM devicetypes WHERE id=716;
 DELETE FROM devicetypes WHERE id=717;
 DELETE FROM devicetypes WHERE id=718;
+DELETE FROM devicetypes WHERE id=719;
 
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (711, 'VMBDMI', 'Velbus - dimmer for resistive or inductive load', 'Velbus', '\'02\', \'05\' or \'12\'');
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (712, 'VMBPIRC', 'Velbus - Ceiling PIR detector module', 'Velbus', '\'02\', \'05\' or \'12\'');
@@ -20,6 +21,7 @@ INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES 
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (716, 'VMBGP2', 'Velbus - two touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (717, 'VMBGP4', 'Velbus - four touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (718, 'VMBGPOD', 'Velbus - Touch panel with Oled display', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (719, 'VMBGP1', 'Velbus - one touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
 
 --
 -- Finally update to 1.0.024
