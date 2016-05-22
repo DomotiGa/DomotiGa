4b9bed10fffd8085d96066095119350c4a93ff43
Alexie <ualex73@gmail.com>
2016-05-22 19:05:44 +0200

    Fixed: PhilipsHue plugin crashed when the device didn't have the 'bri' value, like Osram devices Changed: PhilipsHue plugin logging improved Added: Philips Hue Osram Plug01 device Fixed: Removed 'system' warning from GlobalVar Added: Velbus plugin supports VMBGP1, VMBGP2, VMBGP4, VMB6PBN and VMB8PBU Added: Velbus plugin will auto-sync date/time during start-up

--- upgrade/10023to10024.sql
+++ upgrade/10023to10024.sql
@@ -1,5 +1,19 @@
 
 --
+-- Modify existing Velbus modules description
+--
+
+UPDATE devicetypes SET description='4 channel relay module' WHERE id=334;
+UPDATE devicetypes SET description='4 channel dimmer module' WHERE id=335;
+UPDATE devicetypes SET description='7 channel input' WHERE id=336;
+UPDATE devicetypes SET description='OLED touch panel' WHERE id=337;
+UPDATE devicetypes SET description='Dimmer module' WHERE id=338;
+UPDATE devicetypes SET description='4 channel relay module ' WHERE id=663;
+UPDATE devicetypes SET description='2 channel blind module' WHERE id=664;
+UPDATE devicetypes SET description='4 channel relay module' WHERE id=671;
+UPDATE devicetypes SET description='8 channel pushbutton mo' WHERE id=674;
+
+--
 -- Add Velbus modules
 --
 
@@ -12,16 +26,18 @@ DELETE FROM devicetypes WHERE id=716;
 DELETE FROM devicetypes WHERE id=717;
 DELETE FROM devicetypes WHERE id=718;
 DELETE FROM devicetypes WHERE id=719;
+DELETE FROM devicetypes WHERE id=720;
 
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (711, 'VMBDMI', 'Velbus - dimmer for resistive or inductive load', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (712, 'VMBPIRC', 'Velbus - Ceiling PIR detector module', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (713, 'VMBPIRO', 'Velbus - Outdoor motion, twilight and temperature sensor', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (714, 'VMB8PBU', 'Velbus - 8-channel push-button module', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (715, 'VMB6PBN', 'Velbus - Push-button interface for 6-/4-fold niko push-buttons', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (716, 'VMBGP2', 'Velbus - two touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (717, 'VMBGP4', 'Velbus - four touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (718, 'VMBGPOD', 'Velbus - Touch panel with Oled display', 'Velbus', '\'02\', \'05\' or \'12\'');
-INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (719, 'VMBGP1', 'Velbus - one touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (711, 'VMBDMI', 'dimmer for resistive or inductive load', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (712, 'VMBPIRC', 'Ceiling PIR detector module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (713, 'VMBPIRO', 'Outdoor motion, twilight and temperature sensor', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (714, 'VMB8PBU', '8-channel push-button module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (715, 'VMB6PBN', 'Push-button interface for 6-/4-fold niko push-buttons', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (716, 'VMBGP2', 'two touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (717, 'VMBGP4', 'four touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (718, 'VMBGPOD', 'Touch panel with Oled display', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (719, 'VMBGP1', 'one touch buttons module', 'Velbus', '\'02\', \'05\' or \'12\'');
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (720, 'Osram LIGHTIFY Plug','Osram LIGHTIFY Plug','PhilipsHue','\'1\', \'6\' or \'19\'');
 
 --
 -- Finally update to 1.0.024
