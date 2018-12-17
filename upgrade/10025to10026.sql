563e252cdc4fa638b3b8e1ec6dd22117e17163dd
Alexander <ualex73@users.noreply.github.com>
2018-12-17 10:05:37 +0100

    Changed upgrade file to 1.0.026
    1.0.025 is already released, so we need to put it into 1.0.026.

---
+++ upgrade/10025to10026.sql
@@ -0,0 +1,15 @@
+
+--
+-- Add Devices
+--
+INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (731, 'Etekcity','Etekcity ZAP 5LX Sockets','Energenie5','A01')
+
+--
+-- Finally update to 1.0.026
+--
+
+DELETE FROM version WHERE db='1.0.026';
+
+LOCK TABLES version WRITE;
+INSERT INTO version VALUES (70,'1.0.026');
+UNLOCK TABLES;
