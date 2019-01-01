fbd9017392accc3457a751e922b0b179ff4f33b7
Mark Parker <11211952+msp1974@users.noreply.github.com>
2019-01-01 10:45:06 +0000

    Multi Value Dimming Control (#59)
    * Added: Option for multi value dimming

--- upgrade/10025to10026.sql
+++ upgrade/10025to10026.sql
@@ -11,6 +11,9 @@ DELETE FROM devicetypes WHERE id=731;
 
 INSERT INTO devicetypes (id, name, description, protocol, addressformat) VALUES (731, 'Etekcity','Etekcity ZAP 5LX Sockets','Energenie5','A01')
 
+-- Add new field to devices for multi value dim devices
+ALTER TABLE devices ADD multivaluedim tinyint(1) DEFAULT 0 NOT NULL AFTER dimable;
+
 --
 -- Finally update to 1.0.026
 --
